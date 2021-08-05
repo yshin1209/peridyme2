b0469dad6ad337362cf2be86e642906990d95650
yshin1209 <yshin1209@gmail.com>
2021-08-05 07:19:56 -0700

    Update __init__.py

--- EventGridTrigger/__init__.py
+++ EventGridTrigger/__init__.py
@@ -24,7 +24,7 @@ async def main(event: func.EventGridEvent):
     dbclient = client.Client('wss://peridymegraph.gremlin.cosmos.azure.com:443/','g', 
     message_serializer=serializer.GraphSONSerializersV2d0(),
     username="/dbs/db/colls/Graph1", 
-    password="47ONfPHcunYSxeR8elFB4JpKED2Rei1mFANxpMyfPDOU8tX2ZIE1gNYJ9Pl7NY2DRZ0IouKwuxyy8nqPOqXrQg==")
+    password="")
     
     query = f"g.V('{input_id}').out('function').values('value')"
     callback = dbclient.submitAsync(query)
